35ea6bbd83a0f78b75ee5cd03ae7bc3392faee18
Harsh Prasad <harshisindian.com>
2023-12-10 21:09:44 +0530

    Managing State

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,38 +1,75 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle, faMinusCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
-import JeeraPowder from './Assets/Images/Jeera.jpeg'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlusCircle,
+  faMinusCircle,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import JeeraPowder from "./Assets/Images/Jeera.jpeg";
 
 class CartItem extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       price: 120,
-      title: 'Jeera Powder',
+      title: "Jeera Powder",
       qty: 1,
-      img: JeeraPowder
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
+      img: JeeraPowder,
+    };
   }
+
+  // Form 1
   increaseQuantity = () => {
-    console.log('this', this.state);
+    this.setState({
+      qty: this.state.qty + 1,
+    });
+  };
+
+  // Form 2
+  decreaseQuantity = () => {
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty > 0 ? prevState.qty - 1 : prevState.qty,
+      };
+    });
+  };
+
+  deleteItem = () => {
+    console.log('Delete Button Code Unavailable')
   }
-  render () {
+
+  render() {
     const { price, title, qty, img } = this.state;
+    console.log('render')
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} alt='Item' src={img}/>
+          <img style={styles.image} alt="Item" src={img} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price} </div>
+          <div style={{ color: "#777" }}>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <FontAwesomeIcon className='action-icons' icon={faPlusCircle} size="lg" />
-            <FontAwesomeIcon className='action-icons' icon={faMinusCircle} size="lg" />
-            <FontAwesomeIcon className='action-icons' icon={faTrash} size="lg" />
+            <FontAwesomeIcon
+              className="action-icons"
+              icon={faPlusCircle}
+              size="lg"
+              onClick={this.increaseQuantity}
+            />
+            <FontAwesomeIcon
+              className="action-icons"
+              icon={faMinusCircle}
+              size="lg"
+              onClick={this.decreaseQuantity}
+            />
+            <FontAwesomeIcon
+              className="action-icons"
+              icon={faTrash}
+              size="lg"
+              onClick={this.deleteItem}
+            />
           </div>
         </div>
       </div>
@@ -45,8 +82,8 @@ const styles = {
     height: 120,
     width: 100,
     borderRadius: 4,
-    background: '#ccc'
-  }
-}
+    background: "#ccc",
+  },
+};
 
 export default CartItem;
